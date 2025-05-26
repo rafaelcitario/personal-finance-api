c82d3a15e8a9e1e69e076b4719f53ade8b75279d
Rafael Citario <contato.rafaelgomes@outlook.com>
2025-05-26 13:26:22 -0300

    change file name

--- src/http/controllers/auth.login.controller.ts
+++ src/http/controllers/auth.login.controller.ts
@@ -3,11 +3,11 @@ import { AuthPayload } from '../../interfaces/auth.login.interface';
 import { authLoginService } from '../../services/auth.login.service';
 
 
-export function authLoginController ( req: Request, res: Response ) {
+export async function authLoginController ( req: Request, res: Response ) {
     const { email, password }: AuthPayload = req.body;
     try {
-        authLoginService( { email, password } );
-        res.status( 201 ).send( 'The login was a success!' );
+        const loginAuth = await authLoginService( { email, password } );
+        res.status( 201 ).json( loginAuth );
     } catch ( e ) {
         const typedError = e as Error;
         res.status( 500 ).send( { error: typedError.message } );
