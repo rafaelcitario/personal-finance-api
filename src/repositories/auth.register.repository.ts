e8a308358f74e5e061c7ea5d01cff3dc4f88a02b
Rafael Citario <contato.rafaelgomes@outlook.com>
2025-05-28 14:43:44 -0300

    fix: connect database user create table

--- src/repositories/auth.register.repository.ts
+++ src/repositories/auth.register.repository.ts
@@ -1,14 +1,18 @@
+import { PrismaClient } from '../../generated/prisma';
 import { RegisterPayload } from '../http/controllers/auth.register.controller';
 
 type AuthRepositoryDTO = Pick<RegisterPayload, 'email' | 'name'>;
-export function authRegisterRepository ( data: AuthRepositoryDTO & { password_hash: string; } ) {
+export async function authRegisterRepository ( data: AuthRepositoryDTO & { password_hash: string; } ) {
     const { name, email, password_hash } = data;
-    const database = [{
-        name: 'pimenta',
-        email: 'pimenta@gmail.com.br',
-        password_hash: 'dfc29b6b50bd9c8e2efc95752a54cb7838ef34b68686883c215f5295f7b60141650e753726944678da1eca95a7ebe321475faecc9bc6a9205ebd805adbe7dd77'
-    }];
-    // TODO: add database connection to find login and password
-    const temp = [name, email, password_hash, database];
+    const prisma = new PrismaClient();
+    const user = await prisma.$transaction( [
+        prisma.users.create( {
+            data: {
+                name, email, password_hash
+            }
+        } )
+    ] );
+    console.log( user );
+    return;
 }
 
